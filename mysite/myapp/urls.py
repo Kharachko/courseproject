4ace72243d173b5015009f5f4fd1d70608d08070
Kharachko <volodymyr.kharachko.pp.2022@lpnu.ua>
2024-05-20 02:09:51 +0300

    finish

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "myapp"
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('manga/<int:manga_id>/', views.indexManga, name='indexManga'),
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     #http://127.0.0.1:8000/admin/
     path('admin/', admin.site.urls),
     #http://127.0.0.1:8000/myapp/
-    path("myapp/", include("myapp.urls"))
+    path("myapp/", include("myapp.urls")),
+    #http://127.0.0.1:8000/users/
+    path("users/", include("users.urls"))
 ]
 
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
