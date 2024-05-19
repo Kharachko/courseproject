245a6297d575996a5ed012e37d1eaccee9fd8b32
Kharachko <volodymyr.kharachko.pp.2022@lpnu.ua>
2024-05-19 19:17:07 +0300

    319

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
+from django.db import models
+
 class Manga(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200, default="Unknown Author")
     description = models.TextField(default="No description available")
-
+    genres = models.CharField(max_length=200, default="No genres specified")  # Поле для жанрів
+    ongoing = models.BooleanField(default=True)  # Поле для стану (чи виходить чи завершена)
+    image_file = models.ImageField(blank=True,upload_to='images/', default=None)
+     
     def __str__(self):
         return self.title
+
 
 class Chapter(models.Model):
     manga = models.ForeignKey(Manga, related_name='chapters', on_delete=models.CASCADE)
